92960b2520724be2f1295b7d4b280aa5bf0c33f0
ibnnajjaar <ibnnajjaar@users.noreply.github.com>
2024-05-12 04:12:49 +0000

    Fix styling

--- src/Http/Controllers/OTPController.php
+++ src/Http/Controllers/OTPController.php
@@ -1,15 +1,11 @@
 <?php
 
-
 namespace Javaabu\MobileVerification\Http\Controllers;
 
-
 use Illuminate\View\View;
-use Javaabu\MobileVerification\Traits\HasFormView;
-use Javaabu\MobileVerification\Tests\TestSupport\Models\User;
-use Javaabu\MobileVerification\Traits\CanValidateMobileNumber;
-use Javaabu\MobileVerification\Traits\CanSendVerificationCode;
 use Javaabu\MobileVerification\Contracts\HasMobileNumberValidation;
+use Javaabu\MobileVerification\Traits\CanSendVerificationCode;
+use Javaabu\MobileVerification\Traits\HasFormView;
 
 abstract class OTPController implements HasMobileNumberValidation
 {
