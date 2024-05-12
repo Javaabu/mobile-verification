63b414762e8542b1a4081c386407ffa89ad7e80b
ibnnajjaar <ibnnajjaar@users.noreply.github.com>
2024-05-12 09:52:26 +0000

    Fix styling

--- src/Http/Controllers/UpdateMobileNumberController.php
+++ src/Http/Controllers/UpdateMobileNumberController.php
@@ -8,14 +8,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
+use Javaabu\MobileVerification\Contracts\HasMobileNumber;
 use Javaabu\MobileVerification\Rules\IsValidMobileNumber;
 use Javaabu\MobileVerification\Rules\IsValidToken;
-use Javaabu\MobileVerification\Contracts\HasMobileNumber;
 use Javaabu\MobileVerification\Support\DataObjects\MobileNumberData;
 use Javaabu\MobileVerification\Support\Enums\Countries;
 use Javaabu\MobileVerification\Support\Services\MobileNumberService;
 use Javaabu\MobileVerification\Traits\HasFormView;
-use Javaabu\MobileVerification\Traits\HasUserGuard;
 use Javaabu\MobileVerification\Traits\HasUserType;
 
 abstract class UpdateMobileNumberController
